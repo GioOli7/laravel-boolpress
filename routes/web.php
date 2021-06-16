67a198d0c899751f2c68ce58303252857c8b0fe9
GioOli7 <gio07live@gmail.com>
2021-06-16 15:58:25 +0200

    admin section

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/admin', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->name('admin.')->group(function() {
+    //rotta home admin routes
+    Route::get('/', 'HomeController@index')->name('home');
+});
