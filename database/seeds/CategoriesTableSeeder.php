31eec428b47ca8393611a972d0f0e5126ac911fc
GioOli7 <gio07live@gmail.com>
2021-06-21 16:07:42 +0200

    category

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+use illuminate\Support\Str;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['HTML', 'CSS', 'Javascript', 'PHP'];
+
+        foreach ($categories as $category) {
+
+            $new_category = new Category();
+
+
+            $new_category->name = $category;
+            $new_category->slug = Str::slug($category, '-');
+
+            $new_category->save();
+        }
+    }
+}
